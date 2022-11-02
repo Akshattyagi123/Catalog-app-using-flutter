3f81b08d27bc71fcbe77cd07a3ee7bc586d5c48d
Akshattyagi123 <akshatt961@gmail.com>
2022-11-02 13:42:02 +0530

    Test Commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        primarySwatch: Colors.blue,
         fontFamily: GoogleFonts.lato().fontFamily,
       ),
       darkTheme: ThemeData(
